0d827db7cc51174b82d1188abc736a79045d9c24
Michael Appiah <mke.appiah@gmail.com>
2025-12-07 00:47:38 +0000

    create processing summary class for common models

---
+++ TransTrack.Common.Models/ProcessingSummary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TransTrack.Common.Models
+{
+    public class ProcessingSummary
+    {
+        public string FileName { get; set; }
+        public int TotalRecords { get; set; }
+        public string FileUrl { get; set; }
+        public DateTime DateProcessed { get; set; }
+        public bool IsValid { get; set; }
+        public string ErrorReason { get; set; }
+    }
+}
