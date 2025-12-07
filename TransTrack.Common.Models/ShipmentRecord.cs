e25f5d11c3ff86a908fe2bb88d47765a3fef2119
Michael Appiah <mke.appiah@gmail.com>
2025-12-07 00:46:40 +0000

    create shipment record class for common models

---
+++ TransTrack.Common.Models/ShipmentRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace TransTrack.Common.Models
+{
+    public class ShipmentRecord
+    {
+        public string ShipmentId { get; set; }
+        public string Origin { get; set; }        // For North
+        public string Region { get; set; }        // For South
+        public string Destination { get; set; }
+        public string Date { get; set; }
+        public string Weight { get; set; }        // For North
+        public string LoadType { get; set; }      // For South
+    }
+}
