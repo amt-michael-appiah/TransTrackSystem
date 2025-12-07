e6bc2276a072298e5d1fdadbab5eb37c712310ee
Michael Appiah <mke.appiah@gmail.com>
2025-12-07 00:50:15 +0000

    create logger interface

---
+++ TransTrack.Logging/ILogger.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TransTrack.Logging
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogError(string message);
+        void LogWarning(string message);
+    }
+}
